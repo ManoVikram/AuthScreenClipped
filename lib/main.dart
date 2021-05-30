e62b8a350b51d55ca3fa12cfee9541486894cc50
Mano Vikram <manovik18@gmail.com>
2021-05-30 23:27:56 +0530

    Some minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../screens/welcomeScreen.dart';
-import '../screens/loginScreen.dart';
-import '../screens/signupScreen.dart';
 
 class MyApp extends StatefulWidget {
   @override
